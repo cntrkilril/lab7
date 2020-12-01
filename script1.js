38fce43bd08e60454504709a27ee559432ea6089
Kirill Shalnov <shalkir2002@mail.ru>
2020-12-01 13:08:27 +0300

    Сделал 1 задание

---
+++ script1.js
@@ -0,0 +1,14 @@
+let chislo;
+let stroka;
+let logika;
+let pusto;
+chislo=10;
+stroka="строка"
+logika=true;
+pusto=null;
+console.log(`число "${chislo}" + строка "${stroka}" = "${chislo+stroka}"`); // число к строке, строка к строке
+console.log(`число "${chislo}" + булевый "${logika}" = "${chislo+logika}"`); // число к числу, булевый true к 1 
+console.log(`число "${chislo}" + null "${pusto}" = "${chislo+pusto}"`); // число к числу, null к числу 0
+console.log(`строка "${stroka}" + булевый "${logika}" = "${stroka+logika}"`); // строка к строке, булевый к строке 
+console.log(`строка "${stroka}" + null "${pusto}" = "${stroka+pusto}"`); // строка к строке, null к строке "null"
+console.log(`булевый "${logika}" + null "${pusto}" = "${logika+pusto}"`); // булевый true к 1, null к числу 0 
